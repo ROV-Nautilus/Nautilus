ada6aa4c5c131c532d16dbddc33e4fd639765890
Unknown <dylan.bideau@hotmail.fr>
2017-12-01 15:09:11 +0100

    Last Commit 01/12 2.0
    - Interface améliorer
    - Rangement

---
+++ Interface/Nautilus/src/Carto/PhotoZoom.java
@@ -0,0 +1,43 @@
+package Carto;
+
+import java.awt.Container;
+import java.io.File;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PhotoZoom extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Container win;
+	private String nomPhoto;
+
+	public PhotoZoom(String titre, int x, int y, boolean b, String nomPhoto){
+		super(titre);
+		super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		super.setLocation(x,y);
+		this.setNomPhoto(nomPhoto);
+		
+		/* Construction du Conteneur */
+		win = getContentPane();
+		
+		File fichierPhoto = new File(this.nomPhoto+".jpg");
+		Photo photo = new Photo(fichierPhoto,0,0,2);
+		win.add(photo);
+		super.setSize(640*2, 480*2);
+		super.setVisible(b);
+	}
+
+	public String getNomPhoto() {
+		return nomPhoto;
+	}
+
+	public void setNomPhoto(String nomPhoto) {
+		this.nomPhoto = nomPhoto;
+	}
+	
+}
---
+++ Interface/Nautilus/src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import Camera.Interface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 
+		Interface inter = new Interface("Nautilus",0,0,1366,584,true);
+    }
+	
+}
